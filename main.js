27ae266c6822960099165f8170bba53bf4165b46
grantkendall <137111847+grantkendall@users.noreply.github.com>
2025-03-10 14:42:53 -0500

    Finish game loop
    Game is working as intended and can be played to best of three.

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ const choice = [
     "scissors",
 ];
 
-const computerSelection = getComputerChoice();
-const userSelection = getUserChoice();
-
 let computerScore = 0;
 let userScore = 0;
 
@@ -18,9 +15,16 @@ function getComputerChoice() {
 // Input string is returned when user clicks 'OK'
 function getUserChoice() {
     let userChoice = prompt("What do you choose?");
+    // Defaults to 'rock' if user types incorrectly
+    for (c in choice) {
+        if (userChoice.toLowerCase !== c) {
+            userChoice = choice[0];
+        }
+    }
     return userChoice.toLowerCase();
 };
 
+// Is there a cleaner way to do this?
 function evaluateWinner(computer, user) {
     // Tie condition
     if (computer === user) {
@@ -28,26 +32,26 @@ function evaluateWinner(computer, user) {
     }
     // Computer win conditions
     else if (computer === "paper" && user === "rock") {
-        console.log("Paper beats rock!");
+        console.log("You lose! Paper beats rock!");
         computerScore++;
     }
     else if (computer === "scissors" && user === "paper") {
-        console.log("Scissors beats paper!");
+        console.log("You lose! Scissors beats paper!");
         computerScore++;
     } else if (computer === "rock" && user === "scissors") {
-        console.log("Rock beats scissors!");
+        console.log("You lose! Rock beats scissors!");
         computerScore++;
     }
     // User win conditions
     else if (user === "paper" && computer === "rock") {
-        console.log("Paper beats rock!");
+        console.log("You win! Paper beats rock!");
         userScore++;
     }
     else if (user === "scissors" && computer === "paper") {
-        console.log("Scissors beats paper!");
+        console.log("You win! Scissors beats paper!");
         userScore++;
     } else if (user === "rock" && computer === "scissors") {
-        console.log("Rock beats scissors!");
+        console.log("You win! Rock beats scissors!");
         userScore++;
     }
 };
@@ -57,7 +61,15 @@ function updateScore() {
     console.log(`User score: ${userScore}`);
 };
 
-console.log(`Computer selection: ${computerSelection}`);
-console.log(`User selection: ${userSelection}`);
-evaluateWinner(computerSelection, userSelection);
-updateScore();
+// I need to loop through game() without refreshing the webpage...
+function game() {
+    while (computerScore < 3 || userScore < 3) {
+        if (computerScore === 3 || userScore === 3) {
+            console.log("GAME OVER");
+            break;
+        }
+        evaluateWinner(getComputerChoice(), getUserChoice());  
+    }
+}
+
+game();
