46ec56d4018f7261052d10492f1fbf9666539cae
grantkendall <137111847+grantkendall@users.noreply.github.com>
2025-03-13 14:42:50 -0500

    Configure UI
    Light refactor to change console logs to DOM events. Basic UI is in
    place consisting of buttons and paragraph elements.

--- main.js
+++ main.js
@@ -6,70 +6,85 @@ const choice = [
 
 let computerScore = 0;
 let userScore = 0;
+let userChoice;
+let bestOfScore = 3;
+
+// Consider switch statement for buttons
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const evalBtn = document.querySelector("#eval");
+
+const log = document.querySelector(".log");
+const computerScoreDisplay = document.querySelector(".computer-score");
+const userScoreDisplay = document.querySelector(".user-score");
+
+rockBtn.addEventListener("click", () => userChoice = choice[0]);
+paperBtn.addEventListener("click", () => userChoice = choice[1]);
+scissorsBtn.addEventListener("click", () => userChoice =choice[2]);
+
+evalBtn.addEventListener("click", () => evaluateWinner(getComputerChoice(), userChoice));
 
 function getComputerChoice() {
-    let index = Math.floor(Math.random() * 3)
+    const index = Math.floor(Math.random() * 3)
     return choice[index]
 };
 
-// Input string is returned when user clicks 'OK'
-function getUserChoice() {
-    let userChoice = prompt("What do you choose?");
-    // Defaults to 'rock' if user types incorrectly
-    for (c in choice) {
-        if (userChoice.toLowerCase !== c) {
-            userChoice = choice[0];
-        }
-    }
-    return userChoice.toLowerCase();
-};
-
-// Is there a cleaner way to do this?
 function evaluateWinner(computer, user) {
+    console.log(`Computer choice: ${computer}`);
+    console.log(`User choice: ${user}`);
     // Tie condition
     if (computer === user) {
-        console.log("Tie");
+        log.textContent = "Tie";
     }
     // Computer win conditions
     else if (computer === "paper" && user === "rock") {
-        console.log("You lose! Paper beats rock!");
+        log.textContent = "You lose! Paper beats rock!";
         computerScore++;
     }
     else if (computer === "scissors" && user === "paper") {
-        console.log("You lose! Scissors beats paper!");
+        log.textContent = "You lose! Scissors beats paper!";
         computerScore++;
     } else if (computer === "rock" && user === "scissors") {
-        console.log("You lose! Rock beats scissors!");
+        log.textContent = "You lose! Rock beats scissors!";
         computerScore++;
     }
     // User win conditions
     else if (user === "paper" && computer === "rock") {
-        console.log("You win! Paper beats rock!");
+        log.textContent = "You win! Paper beats rock!";
         userScore++;
     }
     else if (user === "scissors" && computer === "paper") {
-        console.log("You win! Scissors beats paper!");
+        log.textContent = "You win! Scissors beats paper!";
         userScore++;
     } else if (user === "rock" && computer === "scissors") {
-        console.log("You win! Rock beats scissors!");
+        log.textContent = "You win! Rock beats scissors!";
         userScore++;
     }
+    updateScore();
+    userChoice = undefined;
 };
 
 function updateScore() {
-    console.log(`Computer score: ${computerScore}`);
-    console.log(`User score: ${userScore}`);
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    userScoreDisplay.textContent = `User: ${userScore}`;
+    
+    switch (bestOfScore) {
+        case computerScore:
+            log.textContent = "Better luck next time! Play again?";
+        case userScore:
+            log.textContent = "You won!"
+    }
 };
 
-// I need to loop through game() without refreshing the webpage...
-function game() {
-    while (computerScore < 3 || userScore < 3) {
-        if (computerScore === 3 || userScore === 3) {
-            console.log("GAME OVER");
-            break;
-        }
-        evaluateWinner(getComputerChoice(), getUserChoice());  
-    }
-}
+// function game() {
+//     // while (computerScore < 3 || userScore < 3) {
+//     //     if (computerScore === 3 || userScore === 3) {
+//     //         console.log("GAME OVER");
+//     //         break;
+//     //     }
+//         evaluateWinner(getComputerChoice(), getUserChoice());  
+//     }
+// }
 
-game();
+// game();
